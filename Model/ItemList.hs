49ab3ac75bcc954170f0ac5f5ce29f340a1bd5d5
Josh Watzman <jwatzman@jwatzman.org>
2012-11-18 17:23:28 -0800

    Some mutators

--- Model/ItemList.hs
+++ Model/ItemList.hs
@@ -1,8 +1,11 @@
-{-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving,
-    TemplateHaskell #-}
+{-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving, RecordWildCards,
+    TemplateHaskell, TypeFamilies #-}
 
 module Model.ItemList(ItemList(..)) where
 
+import Control.Monad.Reader (ask)
+import Control.Monad.State (get, put)
+import qualified Data.Acid as A
 import Data.Data (Data, Typeable)
 import qualified Data.IxSet as IxS
 import qualified Data.SafeCopy as SC
@@ -23,3 +26,32 @@ initialItemList = ItemList
 		nextItemID = I.ItemID ((2 ^ 33) + 1),
 		items = IxS.empty
 	}
+
+newItem :: Text -> A.Update ItemList I.Item
+newItem body = do
+	il@ItemList{..} <- get
+	let item = I.Item
+		{
+			itemID = nextItemID,
+			status = I.Visible,
+			body = body
+		}
+	put $ ItemList
+		{
+			nextItemID = succ nextItemID,
+			items = IxS.insert item items
+		}
+	return item
+
+itemById :: I.ItemID -> A.Query ItemList (Maybe I.Item)
+itemById id = do
+	ItemList{..} <- ask
+	return $ IxS.getOne $ IxS.getEQ id items
+
+allItems :: A.Query ItemList [I.Item]
+allItems = do
+	ItemList{..} <- ask
+	let visibleItems = IxS.getEQ I.Visible items
+	return $ IxS.toAscList (IxS.Proxy :: IxS.Proxy I.ItemID) visibleItems
+
+$(A.makeAcidic ''ItemList ['newItem, 'itemById, 'allItems])
