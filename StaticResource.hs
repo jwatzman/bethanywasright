b238087f2ec81879a8ed5b4c09bcb6b1f0c42f22
Josh Watzman <jwatzman@jwatzman.org>
2012-11-22 13:13:00 -0800

    Add ID generation to StaticResource monad
    Not strictly static resource related, but IDs are needed for SR to refer to, so
    whatever. :)

--- StaticResource.hs
+++ StaticResource.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module StaticResource(addCss, addJs, runSR, StaticResource, SRData(..)) where
+module StaticResource(
+	SRData(..),
+	StaticResource,
+	addCss,
+	addJs,
+	newId,
+	runSR
+) where
 
 import Control.Monad.State
 
-data SRData = SRData { css :: [String], js :: [String] }
+data SRData = SRData { css :: [String], js :: [String], nextid :: Integer }
 newtype StaticResource a = SR { getSR :: State SRData a }
 
 instance Monad StaticResource where
@@ -21,5 +28,11 @@ addJs newJs = do
 	sr@SRData{..} <- SR get
 	SR $ put $ sr { js = newJs:js }
 
+newId :: StaticResource String
+newId = do
+	sr@SRData{..} <- SR get
+	SR $ put $ sr { nextid = succ nextid }
+	return $ concat ["srid_", show nextid]
+
 runSR :: StaticResource a -> (a, SRData)
-runSR sr = runState (getSR sr) $ SRData { css = [], js = [] }
+runSR sr = runState (getSR sr) $ SRData { css = [], js = [], nextid = 1 }
