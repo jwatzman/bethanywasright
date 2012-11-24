1933f5e991cbed058effd2ac5ef96b2821e68ba3
Josh Watzman <jwatzman@jwatzman.org>
2012-11-24 10:47:10 -0800

    Switch StaticResource to Data.Set
    This just makes more sense and cleans up the interface a bit.

--- StaticResource.hs
+++ StaticResource.hs
@@ -1,10 +1,18 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module StaticResource(addCss, addJs, runSR, StaticResource, SRData(..)) where
+module StaticResource(
+	StaticResource,
+	SRResult(..),
+	addCss,
+	addJs,
+	runSR
+) where
 
 import Control.Monad.State
+import qualified Data.Set as S
 
-data SRData = SRData { css :: [String], js :: [String] }
+data SRData = SRData { cssSet :: S.Set String, jsSet :: S.Set String }
+data SRResult = SRResult { css :: [String], js :: [String] }
 newtype StaticResource a = SR { getSR :: State SRData a }
 
 instance Monad StaticResource where
@@ -14,12 +22,15 @@ instance Monad StaticResource where
 addCss :: String -> StaticResource ()
 addCss newCss = do
 	sr@SRData{..} <- SR get
-	SR $ put $ sr { css = (newCss:css) }
+	SR $ put $ sr { cssSet = S.insert newCss cssSet }
 
 addJs :: String -> StaticResource ()
 addJs newJs = do
 	sr@SRData{..} <- SR get
-	SR $ put $ sr { js = newJs:js }
+	SR $ put $ sr { jsSet = S.insert newJs jsSet }
 
-runSR :: StaticResource a -> (a, SRData)
-runSR sr = runState (getSR sr) $ SRData { css = [], js = [] }
+runSR :: StaticResource a -> (a, SRResult)
+runSR sr =
+	let (r, SRData{..}) =
+		runState (getSR sr) $ SRData { cssSet = S.empty, jsSet = S.empty }
+	in (r, SRResult { css = S.toList cssSet, js = S.toList jsSet })
--- Template/Page.hs
+++ Template/Page.hs
@@ -2,7 +2,6 @@
 
 module Template.Page(render) where
 
-import qualified Data.List
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -11,13 +10,13 @@ import qualified StaticResource as SR
 
 render :: String -> SR.StaticResource H.Html -> H.Html
 render title body =
-	let (bodyMarkup, SR.SRData{..}) = SR.runSR body in
+	let (bodyMarkup, SR.SRResult{..}) = SR.runSR body in
 	H.docTypeHtml $ do
 		H.head $ do
 			H.title $ H.toHtml title
 			renderJs "jquery-1.8.3.min.js"
-			mapM_ renderJs $ Data.List.nub js
-			mapM_ renderCss $ Data.List.nub css
+			mapM_ renderJs js
+			mapM_ renderCss css
 		H.body $ do
 			bodyMarkup
 
