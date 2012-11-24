932657b451fe5176c957771def0c79ffeb6dd45e
Josh Watzman <jwatzman@jwatzman.org>
2012-11-24 01:20:24 -0800

    Add (non-working) edit and delete links

--- Template/ItemList.hs
+++ Template/ItemList.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Template.ItemList(render) where
 
-import Control.Monad (mapM_)
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -9,11 +10,16 @@ import qualified Model.Item as I
 import qualified StaticResource as SR
 
 render :: [I.Item] -> SR.StaticResource H.Html
-render items = return $
-	H.ul $ mapM_ renderItem items
+render items = do
+	SR.addJs "ItemList.js"
+	SR.addCss "ItemList.css"
+	return $ H.ul $ mapM_ renderItem items
 
 renderItem :: I.Item -> H.Html
-renderItem item = H.li ! A.id (itemID item) $ H.toHtml $ I.body item
+renderItem item = H.li ! A.id (itemID item) $ H.ul ! A.class_ "item" $ do
+	H.li $ H.toHtml $ I.body item
+	H.li $ H.a ! A.href "/stuff" ! A.class_ "editLink" $ "Edit"
+	H.li $ H.a ! A.href "/stuff" ! A.class_ "deleteLink" $ "Delete"
 
 itemID :: I.Item -> H.AttributeValue
 itemID item = H.toValue $ concat ["i_", show $ I.getItemID $ I.itemID item]
