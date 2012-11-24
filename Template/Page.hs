5f571a9a2cfa311869345fbed1019139c7e5c2f5
Josh Watzman <jwatzman@jwatzman.org>
2012-11-24 01:07:21 -0800

    Render CSS

--- Template/Page.hs
+++ Template/Page.hs
@@ -2,7 +2,6 @@
 
 module Template.Page(render) where
 
-import Control.Monad (mapM_)
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -15,11 +14,17 @@ render title body =
 	H.docTypeHtml $ do
 		H.head $ do
 			H.title $ H.toHtml title
-			mapM_ renderJs js
-			renderJs "/static/jquery-1.8.3.min.js"
-			-- TODO: render CSS
+			renderJs "jquery-1.8.3.min.js"
+			mapM_ renderJs $ nub js
+			mapM_ renderCss css
 		H.body $ do
 			bodyMarkup
 
+prependPath :: String -> String
+prependPath p = concat ["/static/", p]
+
 renderJs :: String -> H.Html
-renderJs js = H.script ! (A.src $ H.toValue js) $ ""
+renderJs js = H.script ! (A.src $ H.toValue $ prependPath js) $ ""
+
+renderCss :: String -> H.Html
+renderCss css = H.link ! (A.rel "stylesheet") ! (A.href $ H.toValue $ prependPath css)
