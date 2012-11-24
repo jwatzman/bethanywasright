113cc6bdd9629e72dea9a6dc7098ab47703de5e2
Josh Watzman <jwatzman@jwatzman.org>
2012-11-24 01:12:54 -0800

    Oops

--- Template/Page.hs
+++ Template/Page.hs
@@ -2,6 +2,7 @@
 
 module Template.Page(render) where
 
+import qualified Data.List
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -15,7 +16,7 @@ render title body =
 		H.head $ do
 			H.title $ H.toHtml title
 			renderJs "jquery-1.8.3.min.js"
-			mapM_ renderJs $ nub js
+			mapM_ renderJs $ Data.List.nub js
 			mapM_ renderCss css
 		H.body $ do
 			bodyMarkup
