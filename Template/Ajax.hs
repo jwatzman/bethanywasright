8d2f06d8b3d24b4f3e713228ed74d2c230a9a311
Josh Watzman <jwatzman@jwatzman.org>
2012-11-24 18:50:33 -0800

    Add TODO

--- Template/Ajax.hs
+++ Template/Ajax.hs
@@ -6,4 +6,5 @@ import qualified StaticResource as SR
 
 render :: SR.StaticResource H.Html -> H.Html
 render body = bodyMarkup
+	-- TODO do something with the CSS and JS here
 	where (bodyMarkup, _) = SR.runSR body
